f36ce56de64860b8a0c113eddc65e16ea7c2a6f3
Yusuf Ali <ali@yusuf.email>
2014-09-08 23:11:25 -0600

    wrote a small pseudo code on how to write python script

--- trueSkill.py
+++ trueSkill.py
@@ -2,3 +2,19 @@
 
 import trueskill;
 import pymysql;
+
+
+
+# Steps
+
+# 1. load temp table (by gameNumber) into variable
+	# delete from temp table
+
+# 2. load old player information, create if not existant
+
+# 3. Formulate teams
+
+# 4. Apply TrueSkill calculation
+
+# 5. store data into database
+
