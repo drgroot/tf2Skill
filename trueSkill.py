c27b1adfb988c597f8bd9881899c798dc2c71203
Yusuf Ali <ali@yusuf.email>
2014-09-09 19:36:53 -0600

    beginning to write methods to deal with trueSkill calculation

--- trueSkill.py
+++ trueSkill.py
@@ -1,9 +1,57 @@
 #!/usr/bin/python
 
-import trueskill;
+import trueskill as skill;
 import pymysql;
 
+conn = pymysql.connect(host='db4free.net',port=3306,
+	user='trueskill',passwd='something',db='trueskill');
 
+# loads old player information
+# return mu, sigma
+def getPlayerSkill(steamID):
+	cur_gP = conn.cursor(); 
+	cur_gP.execute("SELECT mew,sigma FROM players WHERE steamID = '%s'" % steamID);
+	
+	for person in cur_gP:
+		cur_gP.close();
+		return person[0], person[1]
+
+	cur_gP.execute("INSERT INTO players (steamID) VALUES ('%s') " % steamID)
+	conn.commit();cur_gP.close()
+	return getPlayerSkill(steamID)
+
+
+
+#### MAIN ####
+##############
+
+cur = conn.cursor(); cur2 = conn.cursor();
+cur.execute("SELECT DISTINCT(random) FROM temp");
+
+for randoms in cur:
+	random = randoms[0]; 
+	team_blu = []; team_red = [];
+	time_blu = []; time_red = [];
+	
+	cur2.execute("SELECT * FROM temp WHERE random = %d" % random);
+	
+	# load temp table
+	for player in cur2:
+		steamID = player[0]; time_blue = player[1];
+		time_red = player[2]; result = player[3];
+		
+		# load old player information
+		mew, sigma = getPlayerSkill(steamID);
+		
+		
+		break
+
+	break
+
+#cur2.close();
+
+
+	
 
 # Steps
 
@@ -18,3 +66,6 @@ import pymysql;
 
 # 5. store data into database
 
+
+cur.close();
+conn.close();
