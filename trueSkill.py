2ba29d57085a94b23a13b4e2351a10065fb86e65
Yusuf Ali <ali@yusufali.ca>
2014-10-01 16:00:45 -0400

    modified python trueSkill script to work with sockets and handle multiple client connections. This way it will work dynamically and exactly at the end of the game round. Need to test, ofcourse

--- trueSkill.py
+++ trueSkill.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python
 
-#import trueskill as skill;
-#import pymysql;
-#import ConfigParser;
-import socket;
+import trueskill as skill
+import pymysql
+import ConfigParser
+import socket
 import os
-
-sock = socket.socket()
-sock.bind((socket.gethostname(), 20000))
-sock.listen(5)
-
-while True:
-	c,addr = sock.accept()
-	print sock.recv(1024)	
-
-exit();
+from thread import *
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 config = ConfigParser.ConfigParser();
@@ -25,6 +16,7 @@ host = config.get('database','host')
 user = config.get('database','user')
 passwd = config.get('database','passwd')
 datb = config.get('database','db')
+sock_port = config.get('database','socket_port')
 
 # connect to mysql database
 conn = pymysql.connect(host=host,port=3306,
@@ -58,69 +50,91 @@ rank=%(rank)f WHERE steamID='%(steam)s'" % data)
 	conn.commit()
 	cur_uP.close()
 
+# Function for handling connections 
+# used to work for multiple server connections
+def clientthread(con_client):
+   while True:
+      # recieve data from client
+      gameNumber = con_client.recv(1024)
+
+      if not gameNumber:
+	 break
+      
+      gameNumber = int(gameNumber)
+      print "TrueSkill Calculate group: %d" % gameNumber
+      
+      # start true skill stuff
+      team_blu = []; team_red = []; steam_blu = [];
+      time_blu = []; time_red = []; steam_red = [];
+      result = 0
+      cur = conn.cursor();
+
+      # load stuff from temp table
+      cur.execute("SELECT * from temp WHERE random = %d" % gameNumber)
+
+      for player in cur:
+	 steamID = player[0]; player_blu = float(player[1]);
+	 player_red = float(player[2]); result = int(player[3]);
+	 
+	 # load old player information
+	 mew, sigma = getPlayerSkill(steamID)
+
+	 # append player to correct team
+	 # based on time
+	 if(player_red < player_blu):
+	    team_blu.append(env.create_rating(mu=mew,sigma=sigma))
+	    time_blu.append(player_blu)
+	    steam_blu.append(stemID)
+	 elif(player_red > player_blue):
+	    time_red.append(env.create_rating(mu=mew,sigma=sigma))
+	    time_red.append(player_red)
+	    steam_red.append(steamID)
+      
+      # ensure minimum people are playing
+      if( len(team_red) + len(team_blu) ) < 16:
+	 print "\t\tGroup %d was filtered" % gameNumber
+	 continue
+
+      # apply trueskill calculation
+      if result == 3:
+	 [team_blu, team_red] = env.rate([tuple(team_blu), tuple(team_red)],
+	       weights=[tuple(time_blu), tuple(time_red) ] )
+      else:
+	 [team_red, team_blu] = env.rate([tuple(team_red), tuple(team_blu)],
+	       weights=[tuple(time_red),tuple(time_blu)])
+
+      # update information in the database
+      updatePlayerInfo(team_red, steam_red)
+      updatePlayerInfo(team_blu, steam_blu)
+
+      # drop data from temp table
+      cur_del = conn.cursor()
+      cur_del.execute("DELETE FROM temp WHERE random = %d" % gameNumber)
+      conn.commit(); cur_del.close();
+      cur.close();
+
+   con_client.close()
 
 #### MAIN ####
 ##############
 
-print "-----Running TrueSkill Calculation-----"
+# kill if listening on port 
+os.system("fuser -k -n tcp %d" % int(sock_port))
 
-cur = conn.cursor(); cur2 = conn.cursor();
-cur.execute("SELECT DISTINCT(random) FROM temp");
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.bind( ('',int(sock_port)) )
+sock.listen(10)
 
-for randoms in cur:
-	random = randoms[0]; 
-	team_blu = []; team_red = []; steam_blu = [];
-	time_blu = []; time_red = []; steam_red = [];
-	
-	print "\tCalculating group: %d" % random
+print "TrueSkill Listening for Connections"
 
-	cur2.execute("SELECT * FROM temp WHERE random = %d" % random);
-	
-	# load temp table
-	for player in cur2:
-		steamID = player[0]; player_blue = float(player[1]);
-		player_red = float(player[2]); result = int(player[3]);
-		
-		# load old player information
-		mew, sigma = getPlayerSkill(steamID);
-		 
-		# append player to correct time
-		if ( int(player_red*100) == 0 and int(player_blue*100)==0 ):
-			continue
-		elif ( player_red < player_blue ):
-			team_blu.append(env.create_rating(mu=mew,sigma=sigma))
-			time_blu.append(player_blue)
-			steam_blu.append(steamID)
-		elif ( player_red > player_blue ):
-			team_red.append(env.create_rating(mu=mew,sigma=sigma))
-			time_red.append(player_red)
-			steam_red.append(steamID)
-	
-	# ensure people are playing
-	if (len(team_red) + len(team_blu)) < 16:
-	       # drop group so they stop spamming logs
-	       print "\t\t Group was filtered"
-	       continue
+while 1:
+   # wait to accept connection
+   con,addr = sock.accept()
 
-	# apply TrueSkill calculation
-	if result == 3:
-		[team_blu, team_red] = env.rate([tuple(team_blu), tuple(team_red)],
-	 		weights=[tuple(time_blu), tuple(time_red)])
-	else:
-		[team_red, team_blu] = env.rate([tuple(team_red), tuple(team_blu)],
-	 		weights=[tuple(time_red), tuple(time_blu)])
+   # start new thread to deal with client
+   start_new_thread(clientthread,(con,))
 
-	# update information in the database
-	updatePlayerInfo(team_red,steam_red)
-	updatePlayerInfo(team_blu,steam_blu)
+sock.close()
+conn.close()
 
-	# drop data from temp table
-	cur_del = conn.cursor();
-	cur_del.execute("DELETE FROM temp WHERE random = %d" % random);
-	conn.commit(); cur_del.close();
-
-cur2.close();
-cur.close();
-conn.close();
-
-print "-----Ending TrueSkill Calculation-----"
+exit()
